0d58118b91a28f16ddfd9bb6e96a7c77cc6458d0
Rinat Khabibulin <Rinat.Khabibulin.GDC@ts.fujitsu.com>
2017-09-26 09:46:33 +0300

    Revert "Revert "1""
    This reverts commit f1d4b8b2f8021711d0060f4ebf6b96965dc0a951.

---
+++ WebApplication9/WebApplication9/Scripts/Script.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module("myModule", []).controller("myController", function ($scope, $http) {
+    $http.get("EmployeeService.asmx/GetAllEmployees").then(function (response) {
+        $scope.employees = response.data;
+    });
+});
