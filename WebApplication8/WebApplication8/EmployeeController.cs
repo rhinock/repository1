bfab284783a7dc2c5b2e0705df1369d3617100bd
Rinat Khabibulin <Rinat.Khabibulin.GDC@ts.fujitsu.com>
2017-09-28 12:46:09 +0300

    WebApplication8 / WebApi, javascript, angular

---
+++ WebApplication8/WebApplication8/EmployeeController.cs
@@ -0,0 +1,85 @@
+﻿
+namespace WebApplication8
+{
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Web.Http;
+
+    /// <summary>
+    /// The employee controller.
+    /// </summary>
+    [RoutePrefix("api/employee")]
+    public class EmployeeController : ApiController
+    {
+        /// <summary>
+        /// The get.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IHttpActionResult"/>.
+        /// </returns>
+        public IHttpActionResult Get()
+        {
+            return this.Ok(Repository.Values);
+        }
+
+        /// <summary>
+        /// The get.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IHttpActionResult"/>.
+        /// </returns>
+        [Route("{id}")]
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            return this.Ok(Repository[id]);
+        }
+
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IHttpActionResult"/>.
+        /// </returns>
+        [Route("{id}")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            Repository.Remove(id);
+            return this.StatusCode(HttpStatusCode.NoContent);
+        }
+
+        /// <summary>
+        /// The post.
+        /// </summary>
+        /// <param name="employee">
+        /// The employee.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IHttpActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        public IHttpActionResult Post(Employee employee)
+        {
+            Repository.Add(employee.Id, employee);
+            var location = this.Request.RequestUri + "/" + employee.Id;
+            return this.Created(location, employee);
+        }
+
+        private static readonly Dictionary<int, Employee> Repository = new Dictionary<int, Employee>()
+        {
+           {
+               1, 
+               new Employee() { Id = 1, Age = 23, City = "Kazan", Name = "Andrey" } },
+          { 
+              2, 
+              new Employee() { Id = 2, Age = 233, City = "Samara", Name = "Oleg" } }
+       };
+    }
+}
---
+++ WebApplication8/WebApplication8/WebApiConfig.cs
@@ -0,0 +1,29 @@
+namespace WebApplication8
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net.Http.Headers;
+    using System.Web.Http;
+
+    /// <summary>
+    /// The web api config.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// The register.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
